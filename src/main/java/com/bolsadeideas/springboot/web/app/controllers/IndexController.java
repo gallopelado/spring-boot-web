3c3b37838d4fdd79a3d6ee433adcba9cc1257382
Juan José González <juanftp100@gmail.com>
2019-12-27 17:21:40 -0300

    uso de properties

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -3,6 +3,7 @@ package com.bolsadeideas.springboot.web.app.controllers;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,13 +18,22 @@ import com.bolsadeideas.springboot.web.app.models.Usuario;
 public class IndexController {
 
 	private String titulo;
+	
+	@Value("${texto.indexcontroller.index.titulo}")
+	private String textoIndex;
+	
+	@Value("${texto.indexcontroller.perfil.titulo}")
+	private String textoPerfil;
+	
+	@Value("${texto.indexcontroller.listar.titulo}")
+	private String textoListar;
 
 	// Mapear
 	// @GetMapping(value="/index")
 	// @RequestMapping(value="/index", method = RequestMethod.GET)
 	@GetMapping({ "/index", "/", "/home", "" })
 	public String index(Model model) {
-		this.titulo = "Hola Spring Framework";
+		this.titulo = textoIndex;
 		model.addAttribute("titulo", this.titulo);
 		return "index";
 	}
@@ -35,13 +45,13 @@ public class IndexController {
 		usuario.setApellido("Gonzalez");
 		usuario.setEmail("juanftp100@gmail.com");
 		model.addAttribute("usuario", usuario);
-		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+		model.addAttribute("titulo", textoPerfil + " ".concat(usuario.getNombre()));
 		return "perfil";
 	}
 
 	@RequestMapping("/listar")
 	public String listar(Model model) {
-		this.titulo = "Listado de Usuarios";
+		this.titulo = textoListar;
 //		List<Usuario> usuarios = new ArrayList<>();
 //		usuarios.add(new Usuario("Juan", "Gonzalez", "juanftp100@gmail.com"));
 //		usuarios.add(new Usuario("Celina", "Ramirez", "chili.olimpera@gmail.com"));
